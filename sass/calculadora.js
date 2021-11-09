6313d149094671763c4e93a4ab44969bcba752d4
Jean-Costa_Dev <75809603+jeantheprogrammer@users.noreply.github.com>
2021-11-08 21:50:53 -0300

    final

--- sass/calculadora.js
+++ sass/calculadora.js
@@ -11,3 +11,4 @@ function back() {
     var resultado = document.getElementById("resultado").innerHTML;
     document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1)
 }
+
